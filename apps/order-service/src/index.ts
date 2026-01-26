cf45f1324916e97e71e6883c824411544c9b0ea3
friendgithub-hash <your-email@example.com>
2026-01-26 11:29:54 +0800

    Initial commit

---
+++ apps/order-service/src/index.ts
@@ -0,0 +1,34 @@
+import Fastify from "fastify";
+import { clerkPlugin, getAuth } from "@clerk/fastify";
+
+import { shouldBeUser } from "./middleware/authMiddleware.js";
+
+const fastify = Fastify();
+
+fastify.register(clerkPlugin);
+
+fastify.get("/health", (request, reply) => {
+  return reply.status(200).send({
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+  });
+});
+
+fastify.get("/test", { preHandler: shouldBeUser }, (request, reply) => {
+  return reply.send({
+    message: "Order service is authenticated!",
+    userId: request.userId,
+  });
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 8001 });
+    console.log("Order service is running on 8001");
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
---
+++ apps/product-service/src/index.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { clerkMiddleware, getAuth } from "@clerk/express";
+import { shouldBeUser } from "./middleware/authMiddleware.js";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3002", "http://localhost:3003"],
+    credentials: true,
+  }),
+);
+
+app.use(clerkMiddleware());
+
+app.get("/health", (req: Request, res: Response) => {
+  return res.status(200).json({
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+  });
+});
+
+app.get("/test", shouldBeUser, (req, res) => {
+  res.json({ message: "Product service authenticated", userId: req.userId });
+});
+
+app.listen(8000, () => {
+  console.log("Product service is running on prot 8000");
+});
