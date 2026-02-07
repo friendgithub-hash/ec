41d6af7358190b6cfb80d32b7427f08eb7c2b226
mrfriendch-cloud <friendchg@gmail.com>
2026-02-08 02:48:54 +0800

    feat(admin): update dashboard with order statistics and bar chart

--- packages/types/src/order.ts
+++ packages/types/src/order.ts
@@ -3,3 +3,9 @@ import { OrderSchemaType } from "@repo/order-db";
 export type Ordertype = OrderSchemaType & {
   _id: string;
 };
+
+export type OrderChartType = {
+  month: string;
+  total: number;
+  successful: number;
+};
